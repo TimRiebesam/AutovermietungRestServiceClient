382031a2fa4816e90b5a18a0eaa56e0b1c6062b8
TimRiebesam <42938378+TimRiebesam@users.noreply.github.com>
2019-04-07 21:11:07 +0200

    Zu viel aufgeräumt und rückgängig gemacht. Sorry

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -36,5 +36,76 @@ public class Main {
 
 
 	}
+	
+	 public static String findAllBuchungen() throws UnirestException, WebAppException{
+			BuchungResource buchungResource = new BuchungResource();
+			String buchungenAsString = "";
+
+			try{
+				Buchung[] buchungen = buchungResource.findAll();
+
+				if (buchungen != null) {
+					for (Buchung buchung : buchungen) {
+						buchungenAsString = buchungenAsString + "\n" + buchung.toString();
+					}
+				}
+			}
+			catch(NullPointerException nle){
+				return "Kein Ergebnis gefunden!";
+			}
+			catch(WebAppException e){
+				System.err.println("Es ist eine WebAppException "
+						+ "\nvlt. dies könnte daran liegen, das etwas mit Ihrer Eingabe nicht stimmt"
+						+ "\n\n");
+			}
+			return buchungenAsString;
+		}
+
+		public static String findAllRueckgabe() throws UnirestException, WebAppException{
+			RueckgabeResource rueckgabeResource = new RueckgabeResource();
+			String rueckgabenAsString = "";
+
+			try{
+				Rueckgabe[] rueckgaben = rueckgabeResource.findAll();
+	        //Eine Buchunge zurückgeben
+	        BuchungResource buchungResource = new BuchungResource();
+	        Buchung buchung = buchungResource.findById(3);
+	        System.out.println(buchung);
+
+				if (rueckgaben != null) {
+					for (Rueckgabe rueckgabe : rueckgaben) {
+						rueckgabenAsString = rueckgabenAsString + "\n" + rueckgabe.toString();
+					}
+				}
+			}catch(Exception e) {
+				System.out.println(e);
+			}
+			return rueckgabenAsString;
+		}
+
+
+		public static String findAllAutos() throws UnirestException, WebAppException{
+			AutoResource autoResource = new AutoResource();
+			String autosAsString = "";
+
+			try{
+				Auto[] autos = autoResource.findAll();
+
+				if (autos != null) {
+					for (Auto auto : autos) {
+						autosAsString = autosAsString + "\n" + auto.toString();
+					}
+				}
+			}
+			catch(NullPointerException nle){
+				return "Kein Ergebnis gefunden!";
+			}
+			catch(WebAppException e){
+				System.err.println("Es ist eine WebAppException "
+						+ "\nvlt. dies könnte daran liegen, das etwas mit Ihrer Eingabe nicht stimmt"
+						+ "\n\n");
+			}
+			return autosAsString;
+		}
 
 }
